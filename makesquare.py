f70c243e1aed2ea31d9eed760f534e6c7049b7ab
Sparkery <darkyx65@gmail.com>
2013-11-03 17:58:41 -0500

    Create makesquare.py

---
+++ makesquare.py
@@ -0,0 +1,31 @@
+def index(row, col) :
+        return rows * col + row
+
+radius = 51
+
+border = 300
+
+out = open("creation_s1.ppm", "w")
+
+out.write("P3\n")
+out.write("300\n")
+out.write("300\n")
+out.write("255\n")
+
+for Y in range(border):
+        for X in range(border):
+                if X == 0 or X == border - 1 or Y == 0 or Y == border - 1:
+                        out.write("255 255 255\n")
+                else:
+                        if abs(X - 149) < radius and abs(Y - 149) < radius:
+                                out.write("0 0 0\n")
+                        #if X - radius < 149 and Y - radius < 149:
+                        #        out.write("0 0 0\n")
+                        #elif X + radius > 149 and Y + radius > 149:
+                        #        out.write("0 0 0\n")
+                        else:
+                                out.write("255 255 255\n")
+
+
+out.close()
+
